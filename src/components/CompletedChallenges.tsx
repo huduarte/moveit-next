a3c789ad191b75b07207404617301bab3a49f692
Hudson <hudson.neto@lactec.org.br>
2021-02-24 07:58:09 -0300

    [cod] segunda aula

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges() {
+  return (
+    <div className={styles.compledChallengesContainer}>
+      <span>
+        Desafios completos
+      </span>
+      <span>05</span>
+    </div>
+  )
+}
